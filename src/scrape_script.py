b9936240e693607111e79ff80fef61a6f856deb8
Olamidun <kolapoolamidun@gmail.com>
2021-10-24 20:53:21 +0100

    finished writing the script, yet to test

--- src/scrape_script.py
+++ src/scrape_script.py
@@ -1,3 +1,6 @@
+import csv
+import os
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -6,94 +9,116 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager import driver
-from webdriver_manager.chrome import ChromeDriverManager 
-
-# 'https://www.edmunds.com/inventory/srp.html?inventorytype=used%2Ccpo&wz=73&radius=75'
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-def scrape_car_details():
-    radius = int(input("Enter the radius: "))
-    zipcode = int(input("Enter your zipcode: "))
-    count = 1
-    page = 1
+# This code block is used to write the header for the csv file
+with open('dviz_data.csv', 'w', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    header = ['Name', 'Price', 'VIN', 'Vehicle Summary', 'Top Features & Specs']
+    writer.writerow(header)
 
-    url = f'https://www.edmunds.com/cars-for-sale-by-owner/'
-    # url = f'https://www.edmunds.com/inventory/srp.html?radius={str(radius)}&wz={str(zipcode)}'
 
-    options = Options() # Initialize the options class which is one of the arguments that will be used in webdriver.Chrome class
-    options.headless = False # This is set to false so we can see how the scraping is done via the website UI
-    options.add_experimental_option("detach", True)
+class VehicleInfoClass:
 
-    browser = webdriver.Chrome(ChromeDriverManager().install(), options=options) # class that install chromedriver. ChromeDriverManager().install() ensures the latest version of the chrome driver is installed
-    browser.maximize_window() # This maximizes the browser window to full screen
-    browser.get(url)
-    browser.implicitly_wait(10)
-    browser.set_page_load_timeout(15)
+    # method which makes use of selenium to scrape vehicle info
+    def scrape_vehicle_details(self):
+        zipcode = int(input("Enter your zipcode: "))
+        count = 1 # This is what will be used to differentiate between the vehicles in the vehicle listing page.
 
-    '''
-    This is to ensure that the zipcode input field has been loaded and can be clicked before we attempt to find the input field. We are getting the input field because we need to pass in our own zipcode.
-    '''
-    WebDriverWait(browser, 15).until(EC.element_to_be_clickable((By.NAME, 'zip')))
-    zipcode_form = browser.find_element_by_name('zip') # this finds the zipcode input field
+        url = f'https://www.edmunds.com/cars-for-sale-by-owner/'
 
-    '''
-    
-    ActionChains is used to perform some low-level interaction with the webpage. Such interactions include hover, mouse movements, key press, clicks. It can also be used to fill in a form.
+        options = Options() # Initialize the options class which is one of the arguments that will be used in webdriver.Chrome class
+        options.headless = False # This is set to false so we can see how the scraping is done via the website UI
+        options.add_experimental_option("detach", True)
 
-    In our case we want to clear the default zipcode in the zipcode input field and then use the zipcode entered by the user.
+        browser = webdriver.Chrome(ChromeDriverManager().install(), options=options) # class that install chromedriver. ChromeDriverManager().install() ensures the latest version of the chrome driver is installed
+        browser.maximize_window() # This maximizes the browser window to full screen
+        browser.get(url)
+        browser.set_page_load_timeout(15) # If the page does not load within the value passed into the function, it returns a timeout error.
 
-    '''
-    action = ActionChains(browser)
-    action.move_to_element(zipcode_form).click(zipcode_form).click(zipcode_form).send_keys(Keys.DELETE).send_keys('85001').perform()
 
-    '''
-    
-    While Loop to go back and forth the car listing page and the car details page
-    
-    '''
-    while True:
-        try:
-            '''
-            We are getting each car in the car listing page and then clicking on it. We are doing this by making use of the xpath of the anchor tag, this is so we can click on it and then it takes us to the car details page
+        '''
+        This is to ensure that the zipcode input field has been loaded and can be clicked before we attempt to find the input field. We are getting the input field because we need to pass in our own zipcode.
+        '''
+        WebDriverWait(browser, 15).until(EC.element_to_be_clickable((By.NAME, 'zip')))
+        zipcode_form = browser.find_element_by_name('zip') # this finds the zipcode input field
 
-            '''
-            car = browser.find_element_by_xpath(f"/html/body/div[1]/div/main/div[3]/div[1]/div[1]/div/ul/li[{str(count)}]/div/div[2]/div/h2/a").click()
+        '''
+        
+        ActionChains is used to perform some low-level interaction with the webpage. Such interactions include hover, mouse movements, key press, clicks. It can also be used to fill in a form.
 
-            # car_name_xpath = '//div[@name='overview'/h1'
+        In our case we want to clear the default zipcode in the zipcode input field and then use the zipcode entered by the user.
 
-            # This is used to get the car name in the vehicle details page
-            car_name = browser.find_element_by_css_selector('h1.not-opaque.text-black.d-inline-block.mb-0.size-24')
+        '''
+        action = ActionChains(browser)
+        action.move_to_element(zipcode_form).click(zipcode_form).click(zipcode_form).send_keys(Keys.DELETE).send_keys(str(zipcode)).perform()
 
-            # This is used to get the car name details in the vehicle details page also
-            car_name_details = browser.find_element_by_css_selector('span.not-opaque.text-black')
+        '''
 
-            car_price = browser.find_element_by_xpath("/html/body/div[1]/div/main/div[1]/div[2]/div/div[1]/div[4]/section[1]/div[2]/form/div/div/div/div/div/div[1]/div/div/div[1]/div[1]/div[1]/div[1]/div")
+        
+        
+        While Loop to go back and forth the car listing page and the car details page
+        
+        '''
+        while True:
+            try:
+                '''
+                We are getting each car in the car listing page and then clicking on it. We are doing this by making use of the xpath of the anchor tag, this is so we can click on it and then it takes us to the car details page. This is where I am making use of the count variable I declared above. I am declaring the WevDriverWait to wait for the element that houses the car name to be located by the webdriver before it continue scraping, this is because it some times take a while to load.
 
-            # Gets the VIN number of the vehicle from the vehicle details page
-            vin_number = browser.find_element_by_css_selector("span.mr-1")
+                '''
+                WebDriverWait(browser, 15).until(EC.presence_of_element_located((By.XPATH, f"/html/body/div[1]/div/main/div[3]/div[1]/div[1]/div/ul/li[{str(count)}]/div/div[2]/div/h2/a")))
+                car = browser.find_element_by_xpath(f"/html/body/div[1]/div/main/div[3]/div[1]/div[1]/div/ul/li[{str(count)}]/div/div[2]/div/h2/a").click()
 
-            print(car_name.text)
-            print(car_name_details.text)
-            print(car_price.text)
-            print(vin_number.text)
+                # car_name_xpath = '//div[@name='overview'/h1'
 
-            vehicle_summary = browser.find_elements_by_css_selector("div.m-0.mb-1.row")
-            for info in vehicle_summary:
-                print(info.find_element_by_css_selector("div.col").text)
+                # This is used to get the car name in the vehicle details page
+                car_name = browser.find_element_by_css_selector('h1.not-opaque.text-black.d-inline-block.mb-0.size-24')
 
-            top_features_and_specs = browser.find_elements_by_css_selector("li.mb-0_5")
+                # This is used to get the car name details in the vehicle details page also
+                car_name_details = browser.find_element_by_css_selector('span.not-opaque.text-black')
 
-            for summary in top_features_and_specs:
-                print(summary.text)
+                car_full_name = f'{car_name.text} {car_name_details.text}'
 
-            print(F'--------PRINTING FOR {count} PAGE------------')
+                car_price = browser.find_element_by_css_selector("div.heading-2.mb-0")
 
-            url = f'https://www.edmunds.com/cars-for-sale-by-owner/'
-            browser.get(url)
-            browser.set_page_load_timeout(15)
-            count += 1
+                # Gets the VIN number of the vehicle from the vehicle details page
+                vin_number = browser.find_element_by_css_selector("span.mr-1")
 
-        except Exception as e:
-            print(e)
+                '''the vehicle summary returns a list, so I am looping through the list to get each items in the lsit to extract their test. I am appending them to a list which will be path of the data saved in the csv file'''
+                vehicle_summary = browser.find_elements_by_css_selector("div.m-0.mb-1.row")
+                vehicle_summary_list = []
+                for info in vehicle_summary:
+                    vehicle_summary_list.append(info.find_element_by_css_selector("div.col").text)
 
-scrape_car_details()
+                
+                '''the top features and specs element also returns a list, so I am looping through the list to get each items in the list to extract their test. I am appending them to a list which will be path of the data saved in the csv file'''
+                top_features_and_specs = browser.find_elements_by_css_selector("li.mb-0_5")
+
+                top_features_and_specs_list = []
+                for summary in top_features_and_specs:
+                    top_features_and_specs_list.append(summary.text)
+
+                data = [car_name.text, car_price.text, vin_number.text, vehicle_summary_list, top_features_and_specs_list]
+                with open('dviz_data.csv', 'a', encoding='utf-8', newline='') as file:
+                    writer = csv.writer(file)
+                    writer.writerow(data)
+                print(F'--------PRINTING FOR {count} PAGE------------')
+
+                url = f'https://www.edmunds.com/cars-for-sale-by-owner/'
+                browser.get(url)
+                browser.set_page_load_timeout(15)
+                count += 1 # This increases the count by 1 so selenium knows to click on the next car
+                
+            except Exception as e:
+                print(e)
+
+# This converts this csv file into an excel workbook
+scrapped_vehicle_info = VehicleInfoClass() # Instantiating the class so we can access the method defined in it
+
+scrapped_vehicle_info.scrape_vehicle_details()
+
+read_file = pd.read_csv('dviz_data.csv') # This is a panda function that reads a csv file, which we will convert into an excel workbook
+read_file.to_excel('dviz_data.xlsx', index=None, header=True) # This function converts a csv file into an excel workbook
+
+
